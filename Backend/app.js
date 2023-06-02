5fe593ea1adc51bede33cb17411d07bf8278cf2f
Harsh9425 <harsh.web19@gmail.com>
2023-06-02 17:12:33 +0530

    path changes

--- Backend/app.js
+++ Backend/app.js
@@ -29,8 +29,8 @@ app.use("/api/v1", paymentRoute);
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  // app.use(express.static(path.resolve(__dirname, "./frontend/build")));
-  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 
 // MiddleWare for ErrorHandling
