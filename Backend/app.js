f0691206d7231c5de9271bd6c6f6bf2d7e4778b6
Harsh9425 <harsh.web19@gmail.com>
2023-06-12 11:15:22 +0530

    update cors policy

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const ErrorMiddleWare = require("./Middlewares/error");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
@@ -10,6 +11,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "Backend/.env" });
 }
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
